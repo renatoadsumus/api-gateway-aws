9eaad40fd7e59683aad17c2b6f8da08d71c025f9
renatoadsumus <renatoadsumus@gmail.com>
2020-11-05 18:35:19 -0300

    Inclundo Deploy api

--- create-api.sh
+++ create-api.sh
@@ -4,7 +4,7 @@ set -e
 
 REGION="us-east-1"
 
-APIID=$(aws apigateway import-rest-api --body file://music-dev-swagger.yaml --output text --query id --region ${REGION})
+APIID=$(aws apigateway import-rest-api -body file://music-dev-swagger.yaml --output text --query id --region ${REGION})
 
 PARENTRESOURCEID=$(aws apigateway get-resources --rest-api-id ${APIID} --query "items[?path=='/'].id" --output text --region ${REGION})
 
@@ -13,7 +13,7 @@ PARENTRESOURCEID=$(aws apigateway get-resources --rest-api-id ${APIID} --query "
 
 PUTINTEGRATIONID=$(aws apigateway put-integration --rest-api-id ${APIID} --resource-id ${PARENTRESOURCEID} --http-method GET --type HTTP --integration-http-method GET --uri 'http://orangevalleycaa.org/api/music/' --region ${REGION} --output text)
 
-APIDEPLOY=$(aws apigateway create-deployment --rest-api-id ${APIID} --stage-name dev --region --region ${REGION})
+APIDEPLOY=$(aws apigateway create-deployment --rest-api-id ${APIID} --stage-name dev --region ${REGION} )
 
 echo "ID NEW API GATEWAY: " $APIID
 
